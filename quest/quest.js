525a019f2e6499c63a4d0a8d33e16aa2a4ddbb4b
brookeperkins <brookemmperkins@gmail.com>
2020-07-23 15:02:17 -0700

    render quest

--- quest/quest.js
+++ quest/quest.js
@@ -1,1 +1,13 @@
-console.log('welcome to the QUEST PAGE BABY');
+import allQuests from '../data.js';
+import { findById, renderSection } from '../quest/quest-utils.js';
+
+const main = document.querySelector('main');
+
+// placeholder for query param
+const questId = 'monsters';
+
+const questData = findById(allQuests, questId);
+
+const sectionEl = renderSection(questData);
+
+main.append(sectionEl);
